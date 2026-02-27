4271dde8076a05ff31fe281c152d017318fd99bb
Laura_Reis <150523998+L4ur4r3eiz@users.noreply.github.com>
2026-02-27 08:46:30 -0300

    Add files via upload

---
+++ Aula02/exemplo06.php
@@ -0,0 +1,20 @@
+<?php
+$a = 4;
+$b = 2;
+if ($b == 0) { echo "Não foi possivel rodar seu programa";
+
+} else { 
+    $b != 0;
+    $r = $a / $b;
+    echo $r;
+
+
+}
+
+
+
+
+echo "<br>";
+echo "Aqui continua seu programa..."
+
+?>
---
+++ Aula02/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula02</title>
+</head>
+<body>
+    <h1>Aula 02- html e php</h1>
+
+    <?php 
+    $a = 10;
+    $b = 5;
+    $soma = $a + $b;
+    echo "bem vindo";
+    $nome = "Laura";
+    echo "<p> Bem vindo $nome </p>";
+    echo "<p> Bem vindo" . $nome . "</p>";
+    echo "<p style = 'color:red,'> A soma é: $soma </p>";
+    ?>
+</body>
+</html>
