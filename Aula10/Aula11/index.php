972ba3e7f6ab588e3b816039db5ffedbad895248
Laura_Reis <150523998+L4ur4r3eiz@users.noreply.github.com>
2026-06-02 19:14:11 -0300

    Add files via upload

---
+++ Aula10/Aula11/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Funções PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>Teste das Funções</h1>
+
+    <form action="processa.php" method="post">
+
+        <label>Digite um texto:</label>
+
+        <input type="text" name="texto">
+
+        <br><br>
+
+        <input type="submit" value="Enviar">
+
+    </form>
+
+</div>
+
+</body>
+</html>
